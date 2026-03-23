94eaff8446250c0a15543e5705fc2035723b70b9
RichardPovinelli <richard.povinelli@gmail.com>
2026-03-23 10:25:47 -0500

    Cleaned up code.

--- palindrome_stretch.py
+++ palindrome_stretch.py
@@ -2,15 +2,7 @@
 Goal: Write a program that detects whether a string is a palindrome.
 
 TODO Phase 1:
-- Choose an input string and store it in a variable.
-- Set a flag variable that assumes the string is a palindrome.
-- Use loop logic that compares characters from opposite ends of the string.
-- On each step, compare the left-side character to the matching right-side character.
-- If any pair does not match, update the flag and stop checking further.
-- After the loop, use the flag to decide which message to print.
-- Print whether the string is a palindrome or not.
-- Test with multiple examples, including both palindrome and non-palindrome cases.
-- Add a trace plan to show index pairs checked each iteration.
+- You need to create the TODOs for this phase.
 
 TODO Phase 2:
 - Query a disctionary of words from the internet that has definitions.
